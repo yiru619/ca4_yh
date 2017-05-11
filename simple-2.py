c1a1ef61a31ba62c754b2d3aa6f2865d4cdaa98c
hongzhen29 <yiru@Yirus-MacBook-Air.local>
2017-05-11 22:09:49 +0100

    add get_commits.

--- simple-2.py
+++ simple-2.py
@@ -4,11 +4,34 @@ def read_file(changes_file):
     data = [line.strip() for line in open(changes_file, 'r')]
     return data
 
+def get_commits(data):
+    sep = 72*'-'
+    commits = []
+    current_commit = None
+    index = 0
+    while index < len(data):
+        try:
+            # parse each of the commits and put them into a list of commits
+            details = data[index + 1].split('|')
+            # the author with spaces at end removed.
+            commit = {'revision': details[0].strip(),
+                'author': details[1].strip(),
+                'date': details[2].strip().split(' ')[0],
+                'time': details[2].strip().split(' ')[1],
+                'number_of_lines': details[3].strip().split(' ')[0]
+            }
+            # add details to the list of commits.
+            
+            commits.append(commit)
+            index = data.index(sep, index + 1)
+        except IndexError:
+            break
+    return commits
 
 if __name__ == '__main__':
     # open the file - and read all of the lines.
     changes_file = 'changes_python.txt'
     data = read_file(changes_file)
-
+    commits = get_commits(data)
 
 print data
